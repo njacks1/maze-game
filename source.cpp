9d96b7d27dc2a206ed78c3ce9ce6247488e8aff4
njacks1 <45337815+njacks1@users.noreply.github.com>
2019-04-21 14:24:13 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,19 +1,4 @@
 #include "library.h"
-/*
-void read_maze(){
-ifstream fin("treasure.txt");
-if(fin.fail()){
-cout << endl << "Error opening file\n";
-}
-else{
-for(int j = 0; j<size; j++){
-for(int i = 0; i<size; i++){
-fin >> string[j][i];
-}
-}
-}
-}
-*/
 
 struct coordinates{
 	int xvar, yvar;
@@ -76,10 +61,59 @@ coordinates detection_of_money(string row_of_maze[], coordinates maze_dimensions
 	}
 }
 
-void print_maze(string row_of_maze[], int const size){
-	for (int i = 0; i<size; i++){
-		cout << row_of_maze[i] << endl;
+void draw_box(int i, int j, int const scale, double color[]){
+	move_to(i*scale, j*scale);
+	//cout << endl << color[0] << " " << color[1] << " " << color[2] << endl;
+	set_pen_width(5);
+	set_pen_color(color[0], color[1], color[2]);
+	set_heading_degrees(90);
+	start_shape();
+	draw_distance(scale);	note_position();
+	turn_right_by_degrees(90);
+	draw_distance(scale);	note_position();
+	turn_right_by_degrees(90);
+	draw_distance(scale);	note_position();
+	turn_right_by_degrees(90);
+	draw_distance(scale);	note_position();
+	fill_shape();
+}
+
+void print_maze(string row_of_maze[], coordinates maze_dimensions){
+	int const scale = 50;
+
+
+	//int counter = 0;
+	set_pen_color(4);						//if ur having problems with window size, adjust accordingly
+	double color[3] = { 100, 4, 6 };
+	make_window(maze_dimensions.xvar * scale, maze_dimensions.yvar * scale);
+	cout << endl << maze_dimensions.yvar << endl << maze_dimensions.xvar << endl;
+	for (int j = 0; j < maze_dimensions.yvar; j++){
+		for (int i = 0; i < maze_dimensions.xvar; i++){
+			if (row_of_maze[j][i] == '$'){
+				color[0] = .94;
+				color[1] = .90;
+				color[2] = .55;
+			}
+			else if (row_of_maze[j][i] == '+'){
+				color[0] = 1;
+				color[1] = 1;
+				color[2] = 1;
+			}
+			else if (row_of_maze[j][i] == 'X'){
+				color[0] = 0.08;
+				color[1] = 0.28;
+				color[2] = 0.20;
+				//counter++;			testing if this if-else sequences worked, 148 X's total
+			}
+			else if (row_of_maze[j][i] == '-'){
+				color[0] = 0.86;
+				color[1] = 0.27;
+				color[2] = 0.02;
+			}
+			draw_box(i, j, scale, color);
+		}
 	}
+	//cout << endl << counter << endl;
 }
 
 void main(){
@@ -88,10 +122,11 @@ void main(){
 	coordinates money_pos;
 	int const predefined_size = 1000;
 	string row_of_maze[predefined_size];
-	maze_dimensions = count_and_read_how_many_strings(row_of_maze, predefined_size); //need to count how many strings there are
+	maze_dimensions = count_and_read_how_many_strings(row_of_maze, predefined_size);
 	money_pos = detection_of_money(row_of_maze, maze_dimensions);
 	plus_pos = detection_of_plus(row_of_maze, maze_dimensions);
-	print_maze(row_of_maze, maze_dimensions.yvar);
+	print_maze(row_of_maze, maze_dimensions);
 	cout << "money sign: x = " << money_pos.xvar << " y = " << money_pos.yvar << endl;
 	cout << "plus sign: x = " << plus_pos.xvar << " y = " << plus_pos.yvar << endl;
+	cout << "maze size: x = " << maze_dimensions.xvar << " y = " << maze_dimensions.yvar << endl;
 }
